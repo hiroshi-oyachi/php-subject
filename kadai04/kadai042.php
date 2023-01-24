d030e289ebfa763eff3caa12dd40cb295d6843bb
hiroshi oyachi <167donmai@gmail.com>
2023-01-24 03:11:53 +0000

    first commit

---
+++ kadai04/kadai042.php
@@ -0,0 +1,9 @@
+<?php
+
+$hello = "Hello,";
+$name = "Hiroshi Oyachi";
+$world = "s World!";
+$value = "Hello,";
+$value .= "Hiroshi Oyachi";
+$value .= "s World!";
+echo $value;
---
+++ kadai05/kadai05.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = "Hiroshi Oyachi";
+if ($name < "Hiroshi Oyachi") {
+    echo "私は、あなたの名前です";
+} else {
+    echo "あなたの名前ではありません";
+}
