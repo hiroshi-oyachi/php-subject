c5b09f4d9c36afb519f39ca83655928f6bddb1c3
hiroshi oyachi <167donmai@gmail.com>
2023-02-07 03:27:47 +0000

    修正

--- kadai05/kadai05.php
+++ kadai05/kadai05.php
@@ -1,7 +1,7 @@
 <?php
 
 $name = "Hiroshi Oyachi";
-if ($name < "Hiroshi Oyachi") {
+if ($name === "Hiroshi Oyachi") {
     echo "私は、あなたの名前です";
 } else {
     echo "あなたの名前ではありません";
---
+++ kadai06/kadai06.php
@@ -0,0 +1,10 @@
+<?php
+
+function kakezan($num) {
+    $result = 0;
+        // $result = $result + $i*2;
+    $result = $num * 2;
+    return $result;
+}
+
+echo kakezan(3);
